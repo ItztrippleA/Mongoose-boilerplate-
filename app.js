bfc7e9036b90f83bd2645ffdcaa4564c85f2c55d
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-04-26 15:56:24 +0100

    Tested all CRUD functions in mongoose

--- app.js
+++ app.js
@@ -7,29 +7,41 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB", {
 });
 
 const fruitsSchema = new mongoose.Schema({
-  name: String,
-  rating: Number,
+  name: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10,
+  },
   review: String,
 });
 
+//create a model of the schema or collection
 const Fruit = mongoose.model("Fruit", fruitsSchema);
 
-const apple = new Fruit({
-  name: "Apple",
-  rating: 9,
+// creating multiple objects in the collections
+
+const banana = new Fruit({
+  name: "banana",
+  rating: 10,
   review: "pretty solid",
 });
 
-const orange = new Fruit({ name: "Orange", rating: 10, review: "e try small" });
-const mango = new Fruit({ name: "Mango", rating: 7, review: "e no dey" });
+// const orange = new Fruit({ name: "Orange", rating: 10, review: "e try small" });
+// const mango = new Fruit({ name: "Mango", rating: 7, review: "e no dey" });
 
-Fruit.insertMany([apple, orange, mango], function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("successfully updated the collection");
-  }
-});
+// to insert into a collection many item
+
+// Fruit.insertMany([apple, orange, mango], function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("successfully updated the collection");
+//   }
+// });
 
 const personSchema = new mongoose.Schema({
   name: String,
@@ -42,25 +54,48 @@ const person = new Person({
   name: "Abdullahi Adam",
   age: 13,
 });
-person.save();
-//fruit.save();
+// person.save();
+banana.save();
 
-Fruit.find((err, fruits) => {
+//update an object in a collection
+//takes in a condition then a the parameter to update, and a callback function
+
+// Fruit.updateOne(
+//   { _id: "6086c1c70eebca2d4fddaebe" },
+//   { name: "Peach" },
+//   function (err) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Succesfully updated");
+//     }
+//   }
+// );
+
+//delete an object in a collection
+// Fruit.deleteOne({ name: "Peach" }, function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Succesfully deleted");
+//   }
+// });
+
+//delete multiple objects in a collection
+Person.deleteMany({ name: "Abdullahi Adam" }, function (err) {
   if (err) {
     console.log(err);
   } else {
-    console.log(fruits);
+    console.log("successfully deleted");
   }
 });
 
-const findDocuments = function (db, callback) {
-  // Get the documents collection
-  const collection = db.collection("fruits");
-  // Find some documents
-  collection.find({}).toArray(function (err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits);
-    callback(fruits);
-  });
-};
+//retrieve data from a collection in database fruitsDB
+// Fruit.find((err, fruits) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     mongoose.connection.close();
+//     console.log(fruits);
+//   }
+// });
